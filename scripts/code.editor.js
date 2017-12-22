6766a831abfdfa81f78f02f983046d5db816a0aa
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2017-12-22 08:59:43 -0600

    Add files via upload

---
+++ scripts/code.editor.js
@@ -0,0 +1,11 @@
+function runCode()
+	{
+		var content = document.getElementById('sourceCode').value;
+		var iframe = document.getElementById('targetCode');
+		iframe = (iframe.contentWindow) ? iframe.contentWindow : (iframe.contentDocument.document) ? iframe.contentDocument.document : iframe.contentDocument;
+		iframe.document.open();
+		iframe.document.write(content);
+		iframe.document.close();
+		return false;
+	}
+	runCode();
